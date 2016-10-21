edf5ccb785a8065752d37b64ffd6539d8644b0b6
vaRinia <varinia.ep@gmail.com>
2016-10-21 16:38:12 -0500

    avancce

--- Bicis-locas/js/main.js
+++ Bicis-locas/js/main.js
@@ -1,30 +1,44 @@
-function validateForm() {
+function validateForm() 
+{
 var nombre = document.getElementById("name").value;
 var apellido = document.getElementById("lastname").value;
 var emilio = document.getElementById("input-email").value;
+var contra = document.getElementById("input-password").value;
 var seleccion = document.getElementsByTagName("select")[0].selectedIndex;
 var regexEmilio = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-var regexNombreApellido = !/^[A-Z][a-z]*$/g;
+var regexNombreApellido = /^[ñA-Za-z _]*[ñA-Za-z][ñA-Za-z _]*$/;
 var mensaje = "";
 var err = true;
-	if (!regexNombreApellido.test(nombre)) {
-		mensaje += "Nombre solo letras, primera letra mayúscula; \n";
+
+	if (nombre == null || nombre == "" || !regexNombreApellido.test(nombre)) 
+	{
+		mensaje += "Ingresa nombre, debe contener solo letras, primera letra mayúscula; \n";
 		err = false;
 	}
-	if (!regexNombreApellido.test(apellido)) {
-		mensaje += "Nombre solo letras, primera letra mayúscula; \n";
+	if (apellido == null || apellido == "" || !regexNombreApellido.test(apellido)) {
+		mensaje += "Ingresa apellido, solo letras, primera letra mayúscula; \n";
 		err = false;
 	}
 	if (!regexEmilio.test(emilio)) {
 		mensaje += "Correo inválido; \n";
-		return false;
 		err = false;
 	}
-	if (!err) {
+	if (contra.length < 6 ) {
+		mensaje += "La contraseña debe tener al menos 6 caracteres; \n";
+		err = false;	
+	} else if (contra == "123456" || contra == "password" || contra == "098754") {
+		mensaje += "Ingresa otra contraseña; \n";
+		err = false;
+	}
+	if (seleccion == 0 || seleccion == null) {
+		mensaje += "Elige tu Bici Loca; \n";
+		err = false;	
+	}
+	if (!err) 
+	{
 		alert (mensaje);
 	}
 	return err; 
 }; //fin de la función opcion 2
 
 //(err es falso o verdadero, cuando hubo un error err se volvió falso, si no hubo ningún error, err se asignó desde un momento como verdadero)
-
--- bicis-locas/js/principal.js
+++ bicis-locas/js/principal.js
@@ -1,11 +1,14 @@
 function validateForm() {
+	var nombre = document.getElementById("name").value;
+	if (!/^[A-Z][a-z]*$/g.test(nombre)) {
+		alert("Nombre solo letras, primera letra mayúscula;");
+	}
 	var correo = document.getElementById("input-email").value;
-	var expr = /^([a-zA-Z0-9_\.\])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	if (!expr.test(correo)) {
+	if (!/^([a-zA-Z0-9_\.\])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(correo)) {
 		alert("Error: La dirección de correo " + correo + " es incorrecta.");
 	}
 	var contra = document.getElementById("input-password").value;
-	if (contra.length < 5) {
+	if (contra.length < 6) {
 		alert("Contraseña debe ser mínimo 6 caracteres");
 	} else if (contra == "123456" || contra == "password" || contra == "098754") {
 		alert("Escriba otra contraseña");
@@ -13,4 +16,5 @@ function validateForm() {
 	if (document.getElementsByTagName("select")[0].value == 0) {
 		alert("Selecciona tu tipo de Bici Loca");
 	}
+	return 
 }
